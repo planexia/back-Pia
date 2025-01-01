ea8de95995a22004e0e15536654ade773d4c0ce0
Your Name <rpruiz33@gmail.com>
2025-01-01 17:52:39 -0300

    para planexia email23

--- src/main/java/com/example/demo/EmailController.java
+++ src/main/java/com/example/demo/EmailController.java
@@ -20,7 +20,7 @@ public class EmailController {
     public ResponseEntity<String> sendEmail(@RequestBody EmailRequest emailRequest) {
         try {
             SimpleMailMessage message = new SimpleMailMessage();
-            message.setFrom("rpruiz33@gmail.com"); // Configura correctamente el remitente
+            message.setFrom("planexia.sa@gmail.com"); // Configura correctamente el remitente
             message.setTo("planexia.sa@gmail.com"); // Cambia esto si necesitas múltiples destinatarios
             message.setSubject("Consulta desde el formulario de contacto");
             message.setText("Nombre: " + emailRequest.getName() + "\n"
